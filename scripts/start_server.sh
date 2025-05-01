070e6a8ce370e88e34927f141c42a2e3c9a72dfc
Nisarg153 <nisarg150301@gmail.com>
2025-04-30 22:32:53 -0400

    start server

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,14 +3,11 @@
 echo "🚀 Starting NGINX server..."
 
 # Use full deployment path to avoid "file not found" errors
-DEPLOY_DIR=$(find /opt/codedeploy-agent/deployment-root -type d -name deployment-archive | head -n 1)
 
-# Backup old config if needed
-sudo cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak || true
 
 # Deploy new config
-sudo cp $DEPLOY_DIR/nginx.conf /etc/nginx/nginx.conf
-
+chmod -R 755 /home/ec2-user
+chkconfig nginx on
 # Start and enable NGINX
 sudo systemctl start nginx
 sudo systemctl enable nginx
